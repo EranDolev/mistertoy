deee3f7514edc35b37e24efe27cc66dede8fcd8d
Eran Dolev <doleveran84@gmail.com>
2023-03-14 19:20:00 +0200

    origin

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const corsOptions = {
     origin: [
         'http://127.0.0.1:5173',
         'http://localhost:5173',
+        'http://127.0.0.1:5174',
+        'http://localhost:5174',
     ],
     credentials: true
 }
